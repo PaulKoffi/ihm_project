a8963f2d36761d60455e242e055cf52bdce6bbec
DELABYMael <maeldelabouy@yahoo.fr>
2019-02-19 19:23:48 +0100

    modif pour retourner a l'ancien fxml de creation d'activité

--- src/view/View.java
+++ src/view/View.java
@@ -1,7 +1,7 @@
 package view;
 
 public class View {
-    public static final String XML_FILE = "../resources/CreateAct.fxml";
+    public static final String XML_FILE = "../resources/New_Activity.fxml";
     //public static final String CSS = "resources/calculator.css";
     public static final String LABEL = "Démo MVC";
     public static final int WIDTH = 640;

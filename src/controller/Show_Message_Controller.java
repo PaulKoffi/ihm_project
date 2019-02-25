eeae12fee72c72b51065370d1b7785fa030ab81e
MaelDelaby <maeldelabouy@yahoo.fr>
2019-02-25 16:08:11 +0100

    ajout de l'image

--- src/controller/Show_Message_Controller.java
+++ src/controller/Show_Message_Controller.java
@@ -2,16 +2,22 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import view.View;
 
 public class Show_Message_Controller {
     @FXML
-    private Button buttonOk;
+    private ImageView imageView;
     @FXML
     private Text Tmessage;
+    @FXML
+    private Button buttonOk;
 
     public void showMessage(String message, Stage window){
+        imageView.setImage(new Image(View.SHOW_MESSAGE_ERROR_IMG_FILE));
         Tmessage.setText(message);
         buttonOk.setOnAction(event -> window.close());
     }
